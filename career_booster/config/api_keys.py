5ff4430731a0d2563d84c0a49a0cc57561da02d4
rakeshsudharsan1327 <168258855+rakeshsudharsan1327@users.noreply.github.com>
2025-02-26 11:25:58 +0530

    Update api_keys.py

--- career_booster/config/api_keys.py
+++ career_booster/config/api_keys.py
@@ -9,5 +9,5 @@ GEMINI_API_KEY = "AIzaSyC4aQmveouqu9mQA9zKo9OKITycgiyvfMs"
 # Kaggle API (Download API token from https://www.kaggle.com/account)
 KAGGLE_USERNAME = "sudharsanashokkumar"
 KAGGLE_API_KEY = "917e9d99c8978dcd4f07ff24228d511e"
-OPENAI_API_KEY = "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"
+#OPENAI_API_KEY = "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"
 
